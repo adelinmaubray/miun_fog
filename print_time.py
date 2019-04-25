b1e652f07c31e9c77dbc1a53cd00a5ad1d0d9cb3
adelinmaubray <adelin@maubray.be>
2019-04-25 14:11:25 +0200

    More digits in print_time

--- print_time.py
+++ print_time.py
@@ -1,4 +1,4 @@
 def print_time(start,end):
     hours, rem = divmod(end-start, 3600)
     minutes, seconds = divmod(rem, 60)
-    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+    print("{:0>2}:{:0>2}:{:05.4f}".format(int(hours),int(minutes),seconds))
